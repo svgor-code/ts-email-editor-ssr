6263dec8a6ef11a37d2890ef19f0f5327fc1e487
Konstantin Troitskiy <atr.konst@gmail.com>
2023-06-16 17:47:51 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'production',
+  entry: './main/requestProcessor.js',
+
+  target: 'node',
+
+  externals: [nodeExternals()],
+
+  output: {
+    path: path.resolve('server-build'),
+    filename: 'processor.js',
+    library: 'processor',
+    libraryTarget: 'umd',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
